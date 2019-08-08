e982a743844daae46d8297c1b018bacf7e4d2de6
Mac <ridho@ruangguru.com>
2019-08-08 17:01:40 +0700

    updated code

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const { extractSheets } = require("spreadsheet-to-json");
 const fs = require('fs');
 const ora = require('ora');
 const spinner = ora('translation spreadsheet to json').start();
-const path = require('path');
-const appDir = path.dirname(require.main.filename);
+const appDir = require('app-root-path');
 
 // it assumes that generate file will be in src/i18n
 
